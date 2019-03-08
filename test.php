6c5e0d8e7558bcfb4f22a3a6b8c46b619d55cda9
savio777 <saviopf22@gmail.com>
2019-03-08 19:20:13 -0300

    mais testes

--- test.php
+++ test.php
@@ -7,13 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+    <!-- testes com php -->
     <?php 
+        $oi = 'tá bom kkkk';
         // ultimo comando pode ter ponto e virgula, mas as primeiras não
         //echo('<h1>blz kkkkkkk</h1><br>')
         echo('<h1>blz kkkkkkk</h1><br>'); 
-        echo('<p>de boa kkk</p>')
+        echo('<p>de boa '.$oi.' kkk</p>')
     ?>
-
     <b><?php echo("iae kkkkkk<br>") ?></b>    
     <?php echo('<b>iae negrito tbm kkkk</b>') ?>
 </body>
