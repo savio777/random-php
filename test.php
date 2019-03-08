96a34d061c23a14cef260c2a207fd8cde1d2d935
savio777 <saviopf22@gmail.com>
2019-03-08 19:17:22 -0300

    testes com echo e ponto e virgula

--- test.php
+++ test.php
@@ -7,8 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <?php
-        echo("iae kkkkkk");
-    ?>    
+    <?php 
+        // ultimo comando pode ter ponto e virgula, mas as primeiras não
+        //echo('<h1>blz kkkkkkk</h1><br>')
+        echo('<h1>blz kkkkkkk</h1><br>'); 
+        echo('<p>de boa kkk</p>')
+    ?>
+
+    <b><?php echo("iae kkkkkk<br>") ?></b>    
+    <?php echo('<b>iae negrito tbm kkkk</b>') ?>
 </body>
 </html>
