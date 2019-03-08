246343d01feacbbb8225f5a77e1d6855c59099f1
savio777 <saviopf22@gmail.com>
2019-03-08 19:27:38 -0300

    testes numeros e strings

--- test.php
+++ test.php
@@ -10,10 +10,14 @@
     <!-- testes com php -->
     <?php 
         $oi = 'tá bom kkkk';
+        $n1 = 1;
+        $n2 = '1';
         // ultimo comando pode ter ponto e virgula, mas as primeiras não
         //echo('<h1>blz kkkkkkk</h1><br>')
         echo('<h1>blz kkkkkkk</h1><br>'); 
-        echo('<p>de boa '.$oi.' kkk</p>')
+        echo('<p>de boa '.$oi.' kkk</p>');
+        echo($n1 + $n2 . $n1 - $n2 . $n1 * $n2 . $n1 / $n2);
+        
     ?>
     <b><?php echo("iae kkkkkk<br>") ?></b>    
     <?php echo('<b>iae negrito tbm kkkk</b>') ?>
