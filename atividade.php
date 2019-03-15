3650b5be9d159c9ae9068e4f20811df46897fddc
savio777 <saviopf22@gmail.com>
2019-03-14 21:47:53 -0300

    primeira e segunda questão

---
+++ atividade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>atividade php</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+</head>
+<body>
+    <?php 
+        // primeira questão
+        for($i = 0; $i < 11; $i++){
+            if($i%2 != 0){
+                echo("${i}\n");
+            }
+        }
+
+        echo('<br><br>');
+
+        // segunda questão
+        $num1 = 0;
+        $num2 = 1;
+        if($num1>$num2){
+            echo("${num1} maior que ${num2}");
+        }elseif($num1<$num2){
+            echo("${num1} menor que ${num2}");
+        }elseif($num1==$num2){
+            echo("${num1} igual a ${num2}");
+        }
+
+        echo('<br><br>');
+
+        //terceira questão
+        // desenvolver um algoritmo que percorra um array e some todos os números deste array
+
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
 // teste de redirecionamento para arquivo teste
-header('Location: test.php');
+//header('Location: test.php');
+header('Location: atividade.php');
