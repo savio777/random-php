ecd134c4d80155964d51b98d9211a0963c26c648
savio <saviopf22@gmail.com>
2019-05-14 20:05:36 -0300

    loop para pegar no momento da hora

--- data.php
+++ data.php
@@ -1,11 +1,10 @@
 <?php
 
-$agora = time();
 
-$data = getdate($agora);
-
-echo($data['weekday'].' '. $data['mday'] . '/' . $data['month'] . '/' . $data['year'].' '. $data['hours'].':'.$data['minutes'].':'.$data['seconds']."\n");
-
-echo "\n";
-
-var_dump($data);
+while(TRUE){
+    $agora = time();
+    $data = getdate($agora);    
+    echo($data['weekday'].' '. $data['mday'] . '/' . $data['month'] . '/' . $data['year'].' '. $data['hours'].':'.$data['minutes'].':'.$data['seconds']."\n");
+    echo "\n";
+}
+//var_dump($data);
